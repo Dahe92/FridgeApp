a12d2db5f504a2ea64a3811e701c82a415a50d2a
Tobias Kasch <tobias.kasch@web.de>
2015-10-18 18:36:11 +0200

    added basic list with add functionality

--- client/app/src/main/java/hftl/dwi/se/fridgeapp/Activities/MainActivity.java
+++ client/app/src/main/java/hftl/dwi/se/fridgeapp/Activities/MainActivity.java
@@ -1,23 +1,53 @@
 package hftl.dwi.se.fridgeapp.Activities;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import hftl.dwi.se.fridgeapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listview;
+    final ArrayList<String> list = new ArrayList<String>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-    }
+        listview = (ListView) findViewById(R.id.listView);
+
+        String[] values = new String[]{"Test"};
+
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                list);
+        listview.setAdapter(adapter);
+
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                }
+            });
+        }
 
     public void initScan(View view) {
         IntentIntegrator integrator = new IntentIntegrator(this);
@@ -32,6 +62,6 @@ public class MainActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         //retrieve scan result
         IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-        System.out.println("EAN: " + scanningResult.getContents());
+        ((ArrayAdapter<String>) listview.getAdapter()).add(scanningResult.getContents());
     }
 }
