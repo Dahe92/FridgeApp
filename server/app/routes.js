7c169b447af692a0fb40ef4580119e7d5339d851
hftl-kalle <s134239@hft-leipzig.de>
2015-10-21 18:11:15 +0200

    postsignup added, basic auth geteanino
    Added a /postsignup route for new users, takes x-www-form-urlencoded
    username and password
    -> storage of user data in mongo db, pw hashed by bcrypt.
    fridge needs to be created and stored in user
    
    Changed /geteaninfo?ean=123 to needing basic auth credentials

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,117 +1,113 @@
 // app/routes.js
-module.exports = function(app,$,http) {
+// TODO: more Routes
+
+
+
+module.exports = function(app,passport,$,http,bcrypt,User) {
 
     // =====================================
     // HOME PAGE (with login links) ========
     // =====================================
     app.get('/', function(req, res) {
-        res.send('This is the rest-api for the hftl-fridgeApp'); // load the index.ejs file
+        res.send('This is the rest-api for the hftl-fridgeApp'); 
     });
 
-    // =====================================
-    // LOGIN ===============================
-    // =====================================
-    // show the login form
-    //app.get('/login', function(req, res) {
-
-        // render the page and pass in any flash data if it exists
-    //    res.render('login.ejs', { message: req.flash('loginMessage') }); 
-   // });
-
-    // process the login form
-    // app.post('/login', do all our passport stuff here);
 
     // =====================================
-    // REGISTER ============================
+    // SIGNUP ==============================
     // =====================================
     // register a user via post
-    app.post('/register', function(req, res) {
-		if(true)
-		{
-			// send appropriate response if okay
-			res.render('signup worked');
-		}
-    });
 	
+	app.post('/postsignup',function(req,res){
+		User.findOne({ username :  req.body.username },
+		function(err, user) {
+			// if there are any errors, return the error
+			if (err) {
+				return handleError(err);
+				res.status(500).json({message: "Error during DB search"});
+			}
+			// check to see if theres already a user with that username
+			if (user) {
+				res.status(400).json({message: "There already is user with that username"});
+			} 
+			else{
+
+				// if there is no user with that email
+				// create the user
+				var newUser            = new User();
+
+				// set the user's local credentials
+				newUser.username    = req.body.username;
+				var hash = bcrypt.hash(req.body.password, 10, function(err, hash){
+					if (hash.length>13){
+						newUser.password = hash;
+						// save the user
+						newUser.save(function(err) {
+							if (err){ 
+								return handleError(err);
+								res.status(500).json({message: "Error during hashing"})
+							}
+							res.status(200).json({message :"User signed up"});
+						});
+					}
+					else{
+						res.status(500).json({message: "Error during hashing"})
+					}
+					
+
+				});
+
+			}
+
+		});
+	});    
+    
+
     // =====================================
     // GetEANInfo ==========================
     // =====================================
     // get info for a product via ean and get
-    app.get('/geteaninfo', function(req, res) {
+	
+	
+	app.get('/geteaninfo',
+		passport.authenticate('basic', {session:false}),
+		function(req,res){
 
-		console.log('got eaninfo request');
-		if(req.query.ean!= undefined && req.query.ean!="")
-		{
-			var options= {
-				host: 'opengtindb.org',
-				port:80,
-				path: '/index.php?cmd=ean1&ean='+req.query.ean+'&sq=1',
-				"user-agent": "Mozilla/5.0"
-			};
+			if(req.query.ean!= undefined && req.query.ean!="")
+			{
+				var options= {
+					host: 'opengtindb.org',
+					port:80,
+					path: '/index.php?cmd=ean1&ean='+req.query.ean+'&sq=1',
+					"user-agent": "Mozilla/5.0"
+				};
 
-			console.log(options.path);
-			console.log(options.host);
-			var html='';
-			http.get(options, function(resp){
-				resp.setEncoding('binary');
-				resp.on('data', function(data) {
-					// collect the data chunks to the variable named "html"
-					html += data;
-				}).on('end', function() {
-					console.log('trying jquery')
-					// the whole of webpage data has been collected. parsing time!
-					var prodName = $(html).find('b:contains("Detailname:")').parent().parent().find(':nth-child(2)').text().trim()
-					var prodCat = $(html).find('b:contains("Unterkategorie:")').parent().parent().find(':nth-child(2)').text().trim()
-					var prodTag = $(html).find('b:contains("Name:")').parent().parent().find(':nth-child(2)').text().trim()
-					var productDoc={_id : req.query.ean,name :prodName,mhd:[],category:prodCat,tag :prodTag,unit:"" };
-					res.set({
-						"Content-Type": "application/json; charset=utf-8",
-						"Connection" : "close"
+				var html='';
+				http.get(options, function(resp){
+					resp.setEncoding('binary');
+					resp.on('data', function(data) {
+						// collect the data chunks to the variable named "html"
+						html += data;
+					}).on('end', function() {
+						// the whole of webpage data has been collected. parsing time!
+						var prodName = $(html).find('b:contains("Detailname:")').parent().parent().find(':nth-child(2)').text().trim()
+						var prodCat = $(html).find('b:contains("Unterkategorie:")').parent().parent().find(':nth-child(2)').text().trim()
+						var prodTag = $(html).find('b:contains("Name:")').parent().parent().find(':nth-child(2)').text().trim()
+						var productDoc={_id : req.query.ean,name :prodName,mhd:[],category:prodCat,tag :prodTag,unit:"" };
+						res.set({
+							"Content-Type": "application/json; charset=utf-8",
+							"Connection" : "close"
+						});
+						res.json(productDoc);
 					});
-					res.json(productDoc);
-					console.log(productDoc);
 				});
-			});
+			}
+			else
+			{
+				res.status(400).json({error:'ean tag not found. check querystring'});
+			}
+			
 		}
-		else
-		{
-			res.send('ean not found. check querystring');
-		}
-		
-	});
+	);
 
-
-    // process the signup form
-    // app.post('/signup', do all our passport stuff here);
-
-    // =====================================
-    // PROFILE SECTION =====================
-    // =====================================
-    // we will want this protected so you have to be logged in to visit
-    // we will use route middleware to verify this (the isLoggedIn function)
-    //app.get('/profile', isLoggedIn, function(req, res) {
-    //    res.render('profile.ejs', {
-    //        user : req.user // get the user out of session and pass to template
-    //    });
-    //});
-
-    // =====================================
-    // LOGOUT ==============================
-    // =====================================
-    //app.get('/logout', function(req, res) {
-    //    req.logout();
-    //    res.redirect('/');
-    //});
 };
-
-// route middleware to make sure a user is logged in
-//function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on 
-//    if (req.isAuthenticated())
-  //      return next();
-
-    // if they aren't redirect them to the home page
-    //res.send('Your are not logged in!');
-//}
-
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@
 var express  = require('express');
 var app      = express();
 var port     = 80;
-var mongoClient  = require('mongodb').MongoClient;
+var mongoose  = require('mongoose');
 
 var $        = require('jquery')(require("jsdom-no-contextify").jsdom().parentWindow);
 
@@ -16,34 +16,24 @@ var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
+var User            = require('./app/models/users.js');
+var bcrypt   		= require('bcrypt');
+var BasicStrategy = require('passport-http').BasicStrategy;
 
-//var configDB = require('./config/database.js');
+var configDB = require('./config/database.js');
 
 // configuration ===============================================================
-try{
-var url = 'mongodb://localhost:27017/fridgeApp' // connect to our database
-mongoClient.connect(url, function (err,db){
-	console.log("Connected correctly to server!");
-});
-}
-catch(err){console.log('error: '+err)}
-// require('./config/passport')(passport); // pass passport for configuration
+mongoose.connect(configDB.url);
+
+ require('./config/passport')(passport,BasicStrategy,bcrypt); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
-
-// required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-
 // routes ======================================================================
-require('./app/routes.js')(app,$,http); // load ourroutes and pass in our app and fully configured passport
+require('./app/routes.js')(app,passport,$,http,bcrypt,User); // load ourroutes and pass in our app and fully configured passport
 
 // launch ======================================================================
 app.listen(port);
